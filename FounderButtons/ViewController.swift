2165674d8ab6d2717df5f360a3b2cfc6f11f6863
Yinzhe Ma <mabp@bc.edu>
2019-09-09 16:55:47 -0400

    FounderButton v1 complete

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -14,7 +14,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    
+    @IBAction func googleButtonPressed(_ sender: UIButton) {
+        topLabel.text="Larry Page"
+        bottomLabel.text="Sergey Brin"
+    }
+    
+    
+    @IBAction func rtrButtonPressed(_ sender: UIButton) {
+        topLabel.text="Jennifer Hyman"
+        bottomLabel.text="Jenny Fleiss"
+    }
+    
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        topLabel.text=""
+        bottomLabel.text=""
+    }
+    
 
 }
 
